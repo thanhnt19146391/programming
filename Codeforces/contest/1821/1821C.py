8a23bddba94b44254397fff04c490251fe3a1756
ThanhNT <thanhnt19146391@gmail.com>
2023-04-23 17:33:53 +0700

    new file:   Codeforces/contest/1821/1821C.py

---
+++ Codeforces/contest/1821/1821C.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def check(s):
+    ck = 1
+    for i in range(1, len(s)):
+        if s[i] != s[0]:
+            ck = 0
+            break
+    return ck
+
+def bfs(start):
+    queue = deque([(start, 0)])
+
+    while queue:
+        vertex, cnt = queue.popleft()
+        if check(vertex):
+            res = cnt
+            break
+        if len(vertex) > 1:
+            for k in range(2, len(vertex)):
+                s1 = ''.join(vertex[i] for i in range(0, len(vertex), k))
+                s2 = ''.join(vertex[i] for i in range(1, len(vertex), k))
+                print(s1, s2)
+                queue.extend([(s1, cnt + 1), [s2, cnt + 1]])
+        
+    print(res)
+
+def solve():
+    bfs(s)
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input()
+        solve()
